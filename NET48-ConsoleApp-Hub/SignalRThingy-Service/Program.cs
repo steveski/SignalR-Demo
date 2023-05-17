637f2f6c9271e3da1a6afacfe2d343acdfb4f3c9
APPDEV\SY2635 <stephen.york@dewr.gov.au>
2023-05-17 18:06:24 +1000

    feat: added .NET 4.8 console application hosting a SignalR hub

---
+++ NET48-ConsoleApp-Hub/SignalRThingy-Service/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using System;
+using System.Timers;
+
+namespace SignalRThingy_Service
+{
+    internal class Program
+    {
+        private static Timer _timer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting SignalR Hub");
+            var url = "http://localhost:8080";
+            using(WebApp.Start(url))
+            {
+                _timer = new Timer(10000);
+                _timer.Elapsed += Elapsed;
+                _timer.AutoReset = true;
+                _timer.Enabled = true;
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void Elapsed(object source, ElapsedEventArgs e)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<StatusHub>();
+            context.Clients.All.SendStatus(Environment.MachineName, DateTime.Now.ToString("yyyyMMdd-HHmmss"));
+
+        }
+    }
+}
---
+++ NET48-ConsoleApp-Hub/SignalRThingy-Service/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRThingy_Service.Startup))]
+
+namespace SignalRThingy_Service
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
